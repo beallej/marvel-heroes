6c29679496dc78e27c3edca2066fe8ce556e7cb7
Josie Bealle <josiebealle@gmail.com>
2020-02-18 18:10:42 +0100

    Josie | import sripts

--- scripts/import-elasticsearch.js
+++ scripts/import-elasticsearch.js
@@ -5,5 +5,104 @@ const { Client } = require('@elastic/elasticsearch')
 const esClient = new Client({ node: 'http://localhost:9200' })
 const heroesIndexName = 'heroes'
 
-// TODO
-console.log("TODO ;-)");
+
+async function run () {
+
+    // Création de l'indice
+    esClient.indices.create({ index: heroesIndexName,
+        body: {
+            mappings: {
+                properties: {
+                    suggest: {"type": "completion"}
+                }
+            }
+        }
+
+    }, (err, resp) => {
+        if (err) console.trace(err.message);
+    });
+
+
+
+    let heroes = [];
+    // Read CSV file
+    const BULK_SIZE = 20000;
+    fs.createReadStream('all-heroes.csv')
+        .pipe(csv({
+            separator: ','
+        }))
+        .on('data', (data) => {
+            heroes.push({
+                name: data["name"],
+                id: data["id"],
+                gender: data["gender"],
+                imageUrl: data["imageUrl"],
+                aliases: stringToList(data["aliases"]),
+                secret_identities: stringToList(data["secretIdentities"]),
+                description: data["description"],
+                partners:  stringToList(data["partners"]),
+                universe: data["universe"],
+                first_appearance: data["yearAppearance"],
+                powers:  stringToList(data["powers"]),
+                suggest: [
+                    {
+                        input: data.name,
+                        weight: 10
+                    },
+                    {
+                        input: data.aliases,
+                        weight: 5
+                    },
+                    {
+                        input: data.secret_identities,
+                        weight: 5
+                    }
+                ]
+
+            });
+            if (heroes.length >= BULK_SIZE) {
+                let heroes_full = heroes;
+                heroes = [];
+                esClient.bulk(createBulkInsertQuery(heroes_full), (err, resp) => {
+                    if (err) console.error(err);
+                    else {
+                        // console.log(resp)
+                        console.log(`Inserted ${resp.body.items.length} events`);
+                    }
+                });
+            }
+            // console.log(data);
+        })
+        .on('end', () => {
+
+            esClient.bulk(createBulkInsertQuery(heroes), (err, resp) => {
+                if (err) console.error(err);
+                else console.log(`Inserted ${resp.body.items.length} events`);
+                esClient.close();
+                console.log('Terminated!');
+            });
+        });
+}
+
+// Fonction utilitaire permettant de formatter les données pour l'insertion "bulk" dans elastic
+function createBulkInsertQuery(heroes) {
+    const body = heroes.reduce((acc, hero) => {
+        const { name, id, aliases, secret_identities, description,
+            partners, universe, first_appearance, powers, gender, imageUrl } = hero;
+        acc.push({ index: { _index: heroesIndexName, _type: '_doc', _id: hero.object_id } })
+        acc.push({ name, id, aliases, secret_identities, description,
+            partners, universe, first_appearance, powers, gender, imageUrl })
+        return acc
+    }, []);
+
+    return { body };
+}
+
+function stringToList(st){
+    if (st === "") return [];
+    else {
+        return st.split(",")
+    }
+}
+
+run().catch(console.error);
--- scripts/import-mongo.js
+++ scripts/import-mongo.js
@@ -7,5 +7,51 @@ var mongoUrl = "mongodb://localhost:27017";
 const dbName = "marvel";
 const collectionName = "heroes";
 
-// TODO
-console.log("TODO ;-)");
+
+const insertHeroes = (db, callback) => {
+    const collection = db.collection('heroes');
+
+    const heroes = [];
+    fs.createReadStream('./all-heroes.csv')
+        .pipe(csv())
+        // Pour chaque ligne on créé un document JSON pour l'acteur correspondant
+        .on('data', data => {
+            heroes.push({
+                name: data["name"],
+                id: data["id"],
+                aliases: stringToList(data["aliases"]),
+                secret_identities: stringToList(data["secretIdentities"]),
+                description: data["description"],
+                partners:  stringToList(data["partners"]),
+                universe: data["universe"],
+                first_appearance: data["yearAppearance"],
+                powers:  stringToList(data["powers"])
+
+            });
+        })
+        // A la fin on créé l'ensemble des heroes dans MongoDB
+        .on('end', () => {
+            collection.insertMany(heroes, (err, result) => {
+                callback(result);
+            });
+        });
+}
+
+MongoClient.connect(mongoUrl, (err, client) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    }
+    const db = client.db(dbName);
+    insertHeroes(db, result => {
+        console.log(`${result.insertedCount} heroes inserted`);
+        client.close();
+    });
+});
+
+function stringToList(st){
+    if (st === "") return [];
+    else {
+        return st.split(",")
+    }
+}
